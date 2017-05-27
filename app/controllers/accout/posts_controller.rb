180bf975f291d88a1310b92ea2795c1b414001c0
Wu <457492854@qq.com>
2017-05-28 00:06:13 +0800

    implement my poets

--- app/controllers/accout/posts_controller.rb
+++ app/controllers/accout/posts_controller.rb
@@ -1,8 +1,6 @@
-class Account::PostsController < ApplicationController
+class Accout::PostsController < ApplicationController
   before_action :authenticate_user!
   def index
     @posts = current_user.posts
   end
-
-
 end
